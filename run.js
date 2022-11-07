7b71d163b69943c5c0885030abb84084e2beeadd
anlim <anlimpersonal1@gmail.com>
2022-11-07 16:18:45 -0500

    Add grid size function

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 const container=document.getElementById("container");
 const clearButton=document.getElementById("clear");
-
+const gridSize=document.getElementById("gridSize");
+let desiredSize='';
 
 
 for (i=0; i<256; i++) {
@@ -23,3 +24,30 @@ clearButton.addEventListener('click', ()=> {
     resetBoxes[0].classList.remove('boxActive');
     }
 });
+
+gridSize.addEventListener('click', ()=>{
+    let desiredSize=Number(prompt("What grid size would you like?"));
+    container.style.cssText=`grid-template-columns: repeat(${desiredSize},3vh); grid-template-rows: repeat(${desiredSize}, 3vh);`    
+    if (desiredSize<=100 && desiredSize>0) {
+        removeGrid(container);
+        
+    for (i=0; i<(desiredSize**2); i++) {
+        let box=document.createElement('div');
+        box.classList.add('box');
+        container.appendChild(box);
+
+        box.addEventListener('mouseenter', ()=> {
+            box.classList.remove('box');
+            box.classList.add('boxActive');
+        })
+        }
+    }
+})
+
+
+
+function removeGrid(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
