d65dafbaa37b83810b3a3ffb0e1a742242ea0e40
anlim <anlimpersonal1@gmail.com>
2022-11-14 15:49:40 -0500

    Redo color manipulation to inline instead of class changes

--- run.js
+++ run.js
@@ -1,35 +1,55 @@
 const container=document.getElementById("container");
 const clearButton=document.getElementById("clear");
 const gridSize=document.getElementById("gridSize");
+const rainbow=document.getElementById("rainbow");
+const black=document.getElementById("black");
+const erase=document.getElementById("erase");
+
 let desiredSize='';
+let color="black";
 
-
+//initial grid 
 for (i=0; i<256; i++) {
     let box=document.createElement('div');
     box.classList.add('box');
+    box.addEventListener('mouseover', ()=> {
+        box.style.backgroundColor=`${color}`;
+    });
+    box.style.backgroundColor="lightslategray";
     container.appendChild(box);
-
-    box.addEventListener('mouseenter', ()=> {
-        box.classList.remove('box');
-        box.classList.add('boxActive');
-    })
+    
 }
 
+//button functionality 
+
+black.addEventListener('click', ()=> {
+    color="black";
+});
+
+erase.addEventListener('click', ()=> {
+    color="lightslategray";
+});
+
+rainbow.addEventListener('click', ()=> {
+    color=`${getRandom()}`;
+}
+)
 
 clearButton.addEventListener('click', ()=> {
-    let resetBoxes= document.getElementsByClassName('boxActive');
-    let j=0;
-    while(j<resetBoxes.length) {
-    resetBoxes[0].classList.add('box');
-    resetBoxes[0].classList.remove('boxActive');
-    }
+    let resetBoxes=document.getElementsByClassName('box');
+    for (i=0; i<resetBoxes.length; i++) {
+        resetBoxes[i].style.backgroundColor="lightslategray";
+    };
 });
+
 
 gridSize.addEventListener('click', ()=>{
     userGrid(container); 
 })
 
 
+
+//functions
 
 function removeGrid(container) {
     while (container.firstChild) {
@@ -42,14 +62,13 @@ function createGrid(container, desiredSize) {
     for (k=0; k<(desiredSize**2); k++) {
         let box=document.createElement('div');
         box.classList.add('box');
+        box.style.backgroundColor="lightslategray";
+        box.addEventListener('mouseover', ()=> {
+            box.style.backgroundColor=`${color}`;
+        });
         container.appendChild(box);
-
-        box.addEventListener('mouseenter', ()=> {
-            box.classList.remove('box');
-            box.classList.add('boxActive');
-        })
         }
-}
+    }
 
 function userGrid(container) {
     let desiredSize=Number(prompt("What grid size would you like?"));
@@ -65,3 +84,18 @@ function userGrid(container) {
         userGrid(container);
     }
 }
+
+
+function getRandom(){
+    // 16777215 (decimal) == ffffff in hexidecimal
+    let newColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    
+    // Convert hex to RGB:
+    let rgbColor = newColor.replace('#','');
+    let r = parseInt(rgbColor.substring(0,2), 16);
+    let g = parseInt(rgbColor.substring(2,4), 16);
+    let b = parseInt(rgbColor.substring(4,6), 16);
+    let result = 'rgba(' + r + ',' + g + ',' + b + ')';
+    return result;
+    
+  };
